4837cf741486fa3aaf0a8a54b7f7fb9264f81f9f
Fernando Moraes <fernandomoraes.lopes@gmail.com>
2019-11-26 20:02:57 +0100

    Added flat buttons

--- lib/main.dart
+++ lib/main.dart
@@ -22,17 +22,34 @@ class MyApp extends StatelessWidget {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: <Widget>[
-                Text("+1"),
-                Text("-1"),
+                FlatButton(
+                  onPressed: () {},
+                  child: Text(
+                    "+1",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 40.0,
+                    ),
+                  ),
+                ),
+                FlatButton(
+                  onPressed: () {},
+                  child: Text(
+                    "-1",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 40.0,
+                    ),
+                  ),
+                ),
               ],
             ),
             Text(
               "Enter!",
               style: TextStyle(
-                color: Colors.white,
-                fontStyle: FontStyle.italic,
-                fontSize: 30.0
-              ),
+                  color: Colors.white,
+                  fontStyle: FontStyle.italic,
+                  fontSize: 30.0),
             ),
           ],
         ),
