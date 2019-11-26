312444fefbf68db27fc13ec41afcefbf31c51878
Fernando Moraes <fernandomoraes.lopes@gmail.com>
2019-11-26 19:45:56 +0100

    Added white color as background and removed debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'People Counter',
-      home: Container(),
+      home: Container(
+        color: Colors.white,
+      ),
     );
   }
 }
