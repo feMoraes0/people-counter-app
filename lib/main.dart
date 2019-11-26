4afb56f32337cdf6ee9a2e7b35777963736298c9
Fernando Moraes <fernandomoraes.lopes@gmail.com>
2019-11-26 19:58:53 +0100

    Added basic elements to main file

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,33 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'People Counter',
       home: Container(
-        color: Colors.white,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "People: 0",
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Text("+1"),
+                Text("-1"),
+              ],
+            ),
+            Text(
+              "Enter!",
+              style: TextStyle(
+                color: Colors.white,
+                fontStyle: FontStyle.italic,
+                fontSize: 30.0
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
