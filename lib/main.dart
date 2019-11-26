a03241d1e3cdde6157677f16526d83085ccf243a
Fernando Moraes <fernandomoraes.lopes@gmail.com>
2019-11-26 20:13:48 +0100

    Added background image

--- lib/main.dart
+++ lib/main.dart
@@ -8,51 +8,60 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'People Counter',
-      home: Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              "People: 0",
-              style: TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
+      home: Stack(
+        children: <Widget>[
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "People: 0",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: <Widget>[
-                FlatButton(
-                  onPressed: () {},
-                  child: Text(
-                    "+1",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 40.0,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: FlatButton(
+                      onPressed: () {},
+                      child: Text(
+                        "+1",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 40.0,
+                        ),
+                      ),
                     ),
                   ),
-                ),
-                FlatButton(
-                  onPressed: () {},
-                  child: Text(
-                    "-1",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 40.0,
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: FlatButton(
+                      onPressed: () {},
+                      child: Text(
+                        "-1",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 40.0,
+                        ),
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-            Text(
-              "Enter!",
-              style: TextStyle(
+                ],
+              ),
+              Text(
+                "Enter!",
+                style: TextStyle(
                   color: Colors.white,
                   fontStyle: FontStyle.italic,
-                  fontSize: 30.0),
-            ),
-          ],
-        ),
+                  fontSize: 30.0,
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
